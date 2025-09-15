2aaa7fa3b6e1395e3bb600bcaeb2ee57f5da96e7
TUISTERa <tuistera@gmail.com>
2025-09-15 23:42:41 +0300

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ mkdir -p ./db
 chmod o+rw ./mount
 chmod o+rw ./db
 NOW=`date +%Y%m%d%H%M%S`
-git clone -b master https://github.com/bgerp/bgerp.git
+git clone -b master https://github.com/TUISTERa/bgerp.git
 echo "Building bgerp:$NOW"
 docker build -t "bgerp:$NOW" -t "bgerp:latest" .
